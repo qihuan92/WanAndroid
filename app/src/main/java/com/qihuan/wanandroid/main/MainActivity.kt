061a82e2a5cfa9612ffb2dc758acd99d16db1bf3
qihuan <qihuan92@126.com>
2020-05-19 10:14:20 +0800

    Initial commit

---
+++ app/src/main/java/com/qihuan/wanandroid/main/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.qihuan.wanandroid.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.qihuan.wanandroid.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
