aec6797d932c02e7f78d6b1fe6874e0f1722f970
qihuan <qihuan@fengbangleasing.com>
2020-05-19 10:14:20 +0800

    Initial commit

---
+++ app/src/main/java/com/qihuan/wanandroid/App.kt
@@ -0,0 +1,15 @@
+package com.qihuan.wanandroid
+
+import android.app.Application
+
+/**
+ * App
+ * @author qi
+ * @date 2020/5/19
+ */
+class App : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
