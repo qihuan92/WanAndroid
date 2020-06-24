2b0f1996699f91e1464e2eeb618dd23b2e889878
qihuan <qihuan@fengbangleasing.com>
2020-06-24 16:54:59 +0800

    ViewBinding

--- app/src/main/java/com/qihuan/wanandroid/main/MainActivity.kt
+++ app/src/main/java/com/qihuan/wanandroid/main/MainActivity.kt
@@ -1,13 +1,16 @@
 package com.qihuan.wanandroid.main
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.qihuan.wanandroid.R
+import androidx.appcompat.app.AppCompatActivity
+import com.qihuan.wanandroid.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
