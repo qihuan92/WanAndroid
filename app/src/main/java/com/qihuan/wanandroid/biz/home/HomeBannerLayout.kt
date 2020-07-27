3b6f8e609e68ee7fe7bba337eeec9f950c68ecf7
qihuan <qihuan92@126.com>
2020-07-27 15:53:11 +0800

    首页Banner调整

--- app/src/main/java/com/qihuan/wanandroid/biz/home/HomeBannerLayout.kt
+++ app/src/main/java/com/qihuan/wanandroid/biz/home/HomeBannerLayout.kt
@@ -11,12 +11,10 @@ import androidx.cardview.widget.CardView
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.constraintlayout.widget.ConstraintSet
 import androidx.recyclerview.widget.RecyclerView
+import androidx.viewpager2.widget.ViewPager2
 import com.qihuan.wanandroid.bean.BannerBean
 import com.qihuan.wanandroid.common.ktx.dp
 import com.qihuan.wanandroid.common.ktx.load
-import com.youth.banner.Banner
-import com.youth.banner.adapter.BannerAdapter
-import com.youth.banner.indicator.CircleIndicator
 
 /**
  * BannerLayout
@@ -31,7 +29,8 @@ class HomeBannerLayout(
 ) : ConstraintLayout(context, attrs, defStyleAttr) {
 
     private lateinit var homeBannerAdapter: HomeBannerAdapter
-    private lateinit var homeBanner: Banner<BannerBean, HomeBannerAdapter>
+    private lateinit var viewPager: ViewPager2
+    private var infinite: Boolean = true
 
     init {
         initView()
@@ -45,27 +44,28 @@ class HomeBannerLayout(
     }
 
     private fun initViewPager() {
-        homeBanner = Banner(context)
-        homeBanner.id = View.generateViewId()
-        homeBanner.layoutParams = LayoutParams(0, 0)
-        addView(homeBanner)
+        viewPager = ViewPager2(context).apply {
+            id = View.generateViewId()
+            layoutParams = LayoutParams(0, 0)
+        }
+        addView(viewPager)
 
         val set = ConstraintSet()
         set.clone(this)
-        set.connect(homeBanner.id, ConstraintSet.TOP, id, ConstraintSet.TOP)
-        set.connect(homeBanner.id, ConstraintSet.BOTTOM, id, ConstraintSet.BOTTOM)
-        set.connect(homeBanner.id, ConstraintSet.LEFT, id, ConstraintSet.LEFT)
-        set.connect(homeBanner.id, ConstraintSet.RIGHT, id, ConstraintSet.RIGHT)
-        set.setDimensionRatio(homeBanner.id, "H,16:9")
+        set.connect(viewPager.id, ConstraintSet.TOP, id, ConstraintSet.TOP)
+        set.connect(viewPager.id, ConstraintSet.BOTTOM, id, ConstraintSet.BOTTOM)
+        set.connect(viewPager.id, ConstraintSet.LEFT, id, ConstraintSet.LEFT)
+        set.connect(viewPager.id, ConstraintSet.RIGHT, id, ConstraintSet.RIGHT)
+        set.setDimensionRatio(viewPager.id, "H,16:9")
         set.applyTo(this)
     }
 
     private fun setUpView() {
-        homeBannerAdapter = HomeBannerAdapter()
+        homeBannerAdapter = HomeBannerAdapter(infinite = infinite)
 
-        homeBanner.apply {
+        viewPager.apply {
             adapter = homeBannerAdapter
-            setIndicator(CircleIndicator(context))
+            registerOnPageChangeCallback(OnPageChangeCallback(infinite, this, homeBannerAdapter))
         }
     }
 
@@ -73,65 +73,133 @@ class HomeBannerLayout(
         homeBannerAdapter.setData(bannerList)
     }
 
-    class HomeBannerAdapter :
-        BannerAdapter<BannerBean, HomeBannerAdapter.ViewHolder>(mutableListOf()) {
+    class HomeBannerAdapter(
+        private val itemList: MutableList<BannerBean> = mutableListOf(),
+        private val infinite: Boolean
+    ) : RecyclerView.Adapter<HomeBannerAdapter.ViewHolder>() {
 
-        override fun onCreateHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        companion object {
+            const val INCREASE_COUNT = 2
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
             return ViewHolder(
-                BannerView(
-                    parent.context
-                )
+                BannerView(parent.context)
             )
         }
 
-        override fun onBindView(holder: ViewHolder?, data: BannerBean, position: Int, size: Int) {
-            holder?.bindTo(data)
+        override fun getItemCount(): Int {
+            return getItemCountInternal()
+        }
+
+        override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+            val data = itemList[getRealPosition(position)]
+            holder.bind(data)
         }
 
         fun setData(bannerList: List<BannerBean>) {
-            this.mDatas = bannerList.toMutableList()
+            itemList.clear()
+            if (bannerList.isNotEmpty()) {
+                itemList.addAll(bannerList)
+            }
             notifyDataSetChanged()
         }
 
-        class ViewHolder(private val bannerView: BannerView) : RecyclerView.ViewHolder(bannerView) {
-            fun bindTo(bannerBean: BannerBean) {
-                bannerView.ivBanner.load(bannerBean.imagePath)
+        fun getRealItemCount(): Int {
+            return itemList.size
+        }
+
+        private fun getItemCountInternal(): Int {
+            return if (infinite) {
+                if (getRealItemCount() > 1) {
+                    getRealItemCount() + INCREASE_COUNT
+                } else {
+                    getRealItemCount()
+                }
+            } else {
+                getRealItemCount()
             }
         }
 
-        class BannerView(
-            context: Context,
-            attrs: AttributeSet? = null,
-            defStyleAttr: Int = 0
-        ) : CardView(context, attrs, defStyleAttr) {
-            val ivBanner: ImageView
-
-            init {
-                layoutParams = MarginLayoutParams(MATCH_PARENT, MATCH_PARENT).apply {
-                    setMargins(15f.dp, 10f.dp, 15f.dp, 10f.dp)
-                }
-                preventCornerOverlap = false
-
-                addView(
-                    ConstraintLayout(context).apply {
-                        id = View.generateViewId()
-                        layoutParams = ConstraintLayout.LayoutParams(MATCH_PARENT, MATCH_PARENT)
-
-                        ivBanner = ImageView(context).apply {
-                            id = View.generateViewId()
-                            layoutParams = ConstraintLayout.LayoutParams(0, 0)
-                        }
-                        addView(ivBanner)
-                        val ivSet = ConstraintSet()
-                        ivSet.clone(this)
-                        ivSet.connect(ivBanner.id, ConstraintSet.TOP, id, ConstraintSet.TOP)
-                        ivSet.connect(ivBanner.id, ConstraintSet.BOTTOM, id, ConstraintSet.BOTTOM)
-                        ivSet.connect(ivBanner.id, ConstraintSet.LEFT, id, ConstraintSet.LEFT)
-                        ivSet.connect(ivBanner.id, ConstraintSet.RIGHT, id, ConstraintSet.RIGHT)
-                        ivSet.applyTo(this)
-                    }
-                )
+        private fun getRealPosition(position: Int): Int {
+            var realPosition = 0
+            if (getRealItemCount() > 1) {
+                realPosition = position % getRealItemCount()
             }
+            if (realPosition < 0) {
+                realPosition += getRealItemCount()
+            }
+            return realPosition
+        }
+
+        class ViewHolder(private val bannerView: BannerView) : RecyclerView.ViewHolder(bannerView) {
+            fun bind(bannerBean: BannerBean) {
+                bannerView.ivBanner.load(bannerBean.imagePath)
+            }
+        }
+    }
+
+    class OnPageChangeCallback(
+        private val infinite: Boolean,
+        private val viewPager: ViewPager2,
+        private val adapter: HomeBannerAdapter
+    ) : ViewPager2.OnPageChangeCallback() {
+        private var tempPosition: Int = 0
+
+        override fun onPageSelected(position: Int) {
+            if (adapter.getRealItemCount() > 1) {
+                tempPosition = position
+            }
+        }
+
+        override fun onPageScrollStateChanged(state: Int) {
+            super.onPageScrollStateChanged(state)
+            when (state) {
+                ViewPager2.SCROLL_STATE_DRAGGING -> {
+                    if (infinite) {
+                        if (tempPosition == adapter.getRealItemCount()) {
+                            viewPager.setCurrentItem(0, false)
+                        }
+                    }
+                }
+                else -> {
+                }
+            }
+        }
+    }
+
+    class BannerView(
+        context: Context,
+        attrs: AttributeSet? = null,
+        defStyleAttr: Int = 0
+    ) : CardView(context, attrs, defStyleAttr) {
+        val ivBanner: ImageView
+
+        init {
+            layoutParams = MarginLayoutParams(MATCH_PARENT, MATCH_PARENT).apply {
+                setMargins(15f.dp, 10f.dp, 15f.dp, 10f.dp)
+            }
+            preventCornerOverlap = false
+
+            addView(
+                ConstraintLayout(context).apply {
+                    id = View.generateViewId()
+                    layoutParams = ConstraintLayout.LayoutParams(MATCH_PARENT, MATCH_PARENT)
+
+                    ivBanner = ImageView(context).apply {
+                        id = View.generateViewId()
+                        layoutParams = ConstraintLayout.LayoutParams(0, 0)
+                    }
+                    addView(ivBanner)
+                    val ivSet = ConstraintSet()
+                    ivSet.clone(this)
+                    ivSet.connect(ivBanner.id, ConstraintSet.TOP, id, ConstraintSet.TOP)
+                    ivSet.connect(ivBanner.id, ConstraintSet.BOTTOM, id, ConstraintSet.BOTTOM)
+                    ivSet.connect(ivBanner.id, ConstraintSet.LEFT, id, ConstraintSet.LEFT)
+                    ivSet.connect(ivBanner.id, ConstraintSet.RIGHT, id, ConstraintSet.RIGHT)
+                    ivSet.applyTo(this)
+                }
+            )
         }
     }
 }
