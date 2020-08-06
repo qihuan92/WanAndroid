16b86704e05d85b5259908c16564837331996f68
qihuan <qihuan@fengbangleasing.com>
2020-08-06 09:06:10 +0800

    修改链接跳转逻辑

--- app/src/main/java/com/qihuan/wanandroid/common/ktx/ContextExt.kt
+++ app/src/main/java/com/qihuan/wanandroid/common/ktx/ContextExt.kt
@@ -1,12 +1,16 @@
 package com.qihuan.wanandroid.common.ktx
 
 import android.content.Context
+import android.content.Intent.FLAG_ACTIVITY_NEW_TASK
 import android.net.Uri
 import androidx.browser.customtabs.CustomTabsIntent
 
 @Suppress("UNUSED_PARAMETER")
 fun Context.openBrowser(url: String, title: String = url) {
     val builder = CustomTabsIntent.Builder()
-    val customTabsIntent = builder.build()
-    customTabsIntent.launchUrl(this, Uri.parse(url))
+    val customTabsIntent = builder
+        .setShowTitle(true)
+        .build()
+    customTabsIntent.intent.addFlags(FLAG_ACTIVITY_NEW_TASK)
+    customTabsIntent.launchUrl(this.applicationContext, Uri.parse(url))
 }
