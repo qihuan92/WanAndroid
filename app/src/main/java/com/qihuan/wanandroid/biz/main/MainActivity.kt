4e89ff88b0653f3d0b4ae67d4ec1fa70f7dd6fba
qihuan <qihuan92@126.com>
2020-09-04 15:16:11 +0800

    导航栏适配

--- app/src/main/java/com/qihuan/wanandroid/biz/main/MainActivity.kt
+++ app/src/main/java/com/qihuan/wanandroid/biz/main/MainActivity.kt
@@ -1,14 +1,27 @@
 package com.qihuan.wanandroid.biz.main
 
+import android.app.ActivityOptions
 import android.os.Bundle
+import android.view.ViewGroup
 import android.widget.Toast
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.commit
+import androidx.core.view.marginBottom
+import androidx.core.view.updateLayoutParams
+import androidx.core.view.updatePadding
+import androidx.recyclerview.widget.DefaultItemAnimator
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.qihuan.wanandroid.R
-import com.qihuan.wanandroid.biz.home.HomeFragment
+import com.qihuan.wanandroid.biz.home.HomeViewModel
+import com.qihuan.wanandroid.biz.home.adapter.ArticleItemViewBinder
+import com.qihuan.wanandroid.biz.home.adapter.HomeBannerViewDelegate
+import com.qihuan.wanandroid.biz.home.adapter.HomeTitleViewBinder
+import com.qihuan.wanandroid.biz.home.adapter.ModuleItemViewBinder
+import com.qihuan.wanandroid.biz.search.SearchActivity
 import com.qihuan.wanandroid.common.ApiResult
-import com.qihuan.wanandroid.common.ktx.transparentStatusBar
-import com.qihuan.wanandroid.common.ktx.viewBinding
+import com.qihuan.wanandroid.common.adapter.PageMultiTypeAdapter
+import com.qihuan.wanandroid.common.ktx.*
 import com.qihuan.wanandroid.common.net.handleEvent
 import com.qihuan.wanandroid.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -18,23 +31,98 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private val binding by viewBinding(ActivityMainBinding::inflate)
+    private val viewModel by viewModels<HomeViewModel>()
+    private lateinit var adapter: PageMultiTypeAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        transparentStatusBar()
+        applyEdgeToEdge()
         setContentView(binding.root)
         initView()
+        bindView()
         bindEvent()
     }
 
     private fun initView() {
-        supportFragmentManager.commit {
-            replace(R.id.layout_content, obtainViewFragment())
+        adaptNavigationBar()
+
+        adapter = PageMultiTypeAdapter()
+        adapter.register(HomeBannerViewDelegate(this))
+        adapter.register(ArticleItemViewBinder())
+        adapter.register(HomeTitleViewBinder())
+        adapter.register(ModuleItemViewBinder())
+
+        val layoutManager = LinearLayoutManager(this)
+        binding.rvList.layoutManager = layoutManager
+        binding.rvList.itemAnimator = DefaultItemAnimator()
+        binding.rvList.adapter = adapter
+        binding.rvList.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                super.onScrolled(recyclerView, dx, dy)
+                if (layoutManager.findFirstCompletelyVisibleItemPosition() == 0) {
+                    binding.fabTop.hideInvisible()
+                }
+            }
+        })
+
+        binding.refreshLayout.apply {
+            setProgressViewOffset(true, 50f.dp, 100f.dp)
+            setDefaultColors()
+
+            setOnRefreshListener {
+                viewModel.refresh()
+            }
+        }
+
+        adapter.setOnLoadMoreListener {
+            viewModel.loadMore()
+        }
+
+        binding.fabTop.setOnClickListener {
+            layoutManager.scrollToPositionWithOffset(0, 0)
+        }
+
+        binding.layoutSearch.setOnClickListener {
+            startActivity(
+                buildIntent<SearchActivity>(this) {},
+                ActivityOptions.makeSceneTransitionAnimation(
+                    this,
+                    binding.layoutSearch,
+                    getString(R.string.transition_name_search)
+                ).toBundle()
+            )
         }
     }
 
-    private fun obtainViewFragment() =
-        supportFragmentManager.findFragmentById(R.id.layout_content) ?: HomeFragment()
+    private fun adaptNavigationBar() {
+        // 顶部 Padding 处理
+        binding.root.setOnApplyWindowInsetsListener { view, insets ->
+            view.updatePadding(top = insets.systemWindowInsetTop)
+            insets
+        }
+
+        val fabTopMarginBottom = binding.fabTop.marginBottom
+        // FAB 适配 NavigationBar
+        binding.fabTop.setOnApplyWindowInsetsListener { view, insets ->
+            view.updateLayoutParams {
+                (this as ViewGroup.MarginLayoutParams).setMargins(
+                    leftMargin,
+                    topMargin,
+                    rightMargin,
+                    fabTopMarginBottom + insets.systemWindowInsetBottom
+                )
+            }
+            insets
+        }
+    }
+
+    private fun bindView() {
+        viewModel.listLiveData.observe(this, {
+            binding.refreshLayout.isRefreshing = false
+            adapter.loadMoreComplete()
+            adapter.items = it
+        })
+    }
 
     private fun bindEvent() {
         // todo 全局异常处理
