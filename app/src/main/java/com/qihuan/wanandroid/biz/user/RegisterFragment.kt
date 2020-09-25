a23439968ccaf4536fa3824d787995c6d2959e52
qihuan <qihuan92@126.com>
2020-09-25 10:55:46 +0800

    注册页面

--- app/src/main/java/com/qihuan/wanandroid/biz/user/RegisterFragment.kt
+++ app/src/main/java/com/qihuan/wanandroid/biz/user/RegisterFragment.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import com.google.android.material.snackbar.Snackbar
 import com.qihuan.wanandroid.R
 import com.qihuan.wanandroid.common.ktx.viewBinding
+import com.qihuan.wanandroid.common.net.ApiResult
 import com.qihuan.wanandroid.databinding.FragmentRegisterBinding
 
 /**
@@ -19,5 +21,20 @@ class RegisterFragment : Fragment(R.layout.fragment_register) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.viewModel = viewModel
+        bindView()
+    }
+
+    private fun bindView() {
+        viewModel.loginResult.observe(viewLifecycleOwner) {
+            when (it) {
+                is ApiResult.Success -> {
+                    requireActivity().onBackPressed()
+                }
+                is ApiResult.Error -> {
+                    Snackbar.make(binding.root, it.error.toString(), Snackbar.LENGTH_SHORT).show()
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/qihuan/wanandroid/biz/user/RegisterViewModel.kt
+++ app/src/main/java/com/qihuan/wanandroid/biz/user/RegisterViewModel.kt
@@ -1,7 +1,14 @@
 package com.qihuan.wanandroid.biz.user
 
+import androidx.databinding.ObservableField
 import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.qihuan.wanandroid.R
+import com.qihuan.wanandroid.common.SingleLiveEvent
+import com.qihuan.wanandroid.common.net.ApiResult
+import com.qihuan.wanandroid.utils.Strings
+import kotlinx.coroutines.launch
 
 /**
  * RegisterViewModel
@@ -11,4 +18,39 @@ import androidx.lifecycle.ViewModel
 class RegisterViewModel @ViewModelInject constructor(
     private val repository: UserRepository
 ) : ViewModel() {
+    val userName by lazy { ObservableField<String>() }
+    val password by lazy { ObservableField<String>() }
+    val confirmPassword by lazy { ObservableField<String>() }
+    val userNameError by lazy { ObservableField<String>() }
+    val passwordError by lazy { ObservableField<String>() }
+    val confirmPasswordError by lazy { ObservableField<String>() }
+    val loginResult by lazy { SingleLiveEvent<ApiResult<Any>>() }
+
+    fun register() {
+        userNameError.set(null)
+        val userNameValue = userName.get()
+        if (userNameValue.isNullOrBlank()) {
+            userNameError.set(Strings.get(R.string.placeholder_user_name))
+            return
+        }
+
+        passwordError.set(null)
+        val passwordValue = password.get()
+        if (passwordValue.isNullOrBlank()) {
+            passwordError.set(Strings.get(R.string.placeholder_text_password))
+            return
+        }
+
+        confirmPasswordError.set(null)
+        val confirmPasswordValue = confirmPassword.get()
+        if (confirmPasswordValue.isNullOrBlank()) {
+            confirmPasswordError.set(Strings.get(R.string.placeholder_text_confirm_password))
+            return
+        }
+
+        viewModelScope.launch {
+            loginResult.value =
+                repository.register(userNameValue, passwordValue, confirmPasswordValue)
+        }
+    }
 }
