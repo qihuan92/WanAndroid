2d39a2bfd5265f104cd6c996845cfe5f77bb3e32
qihuan <qihuan92@126.com>
2020-09-25 17:37:05 +0800

    标签修改

--- app/src/main/java/com/qihuan/wanandroid/common/ktx/TextExt.kt
+++ app/src/main/java/com/qihuan/wanandroid/common/ktx/TextExt.kt
@@ -1,6 +1,11 @@
 package com.qihuan.wanandroid.common.ktx
 
+import android.text.SpannableStringBuilder
+import android.text.TextPaint
+import android.text.style.ClickableSpan
+import android.view.View
 import android.widget.TextView
+import androidx.core.text.inSpans
 import androidx.core.view.isVisible
 
 /**
@@ -16,3 +21,18 @@ fun TextView.showText(value: CharSequence) {
         text = value
     }
 }
+
+inline fun SpannableStringBuilder.click(
+    crossinline onClick: (View) -> Unit,
+    isUnderlineText: Boolean = true,
+    builderAction: SpannableStringBuilder.() -> Unit
+) = inSpans(object : ClickableSpan() {
+    override fun onClick(widget: View) {
+        onClick.invoke(widget)
+    }
+
+    override fun updateDrawState(ds: TextPaint) {
+        ds.color = ds.linkColor
+        ds.isUnderlineText = isUnderlineText
+    }
+}, builderAction = builderAction)
